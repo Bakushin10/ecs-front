59ba912c872b2f71e5294428260e5054b71931be
shin nagai <shnnagai@gmail.com>
2021-07-02 15:23:20 +0900

    update build prod setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   isDebugMode: true,
   production: true,
-  fnolEndpoint: "/api/v1/"
+  fnolEndpoint: "http://ecs-alb-187568801.ap-northeast-1.elb.amazonaws.com/api/v1/fnol"
 };
